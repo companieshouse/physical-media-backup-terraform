2e9750379f678bb0f0093b6acce9e5850cfe9fb2
Marc Ransome <mransome@companieshouse.gov.uk>
2023-03-22 10:20:28 +0000

    Add S3 bucket versioning

--- groups/storage/s3.tf
+++ groups/storage/s3.tf
@@ -8,6 +8,13 @@ resource "aws_s3_bucket_acl" "data" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.data.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
   bucket = aws_s3_bucket.data.id
 
