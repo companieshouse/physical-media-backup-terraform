3803c3f3ea2dac9f8964d050c913e3e41d581052
Marc Ransome <mransome@companieshouse.gov.uk>
2023-03-22 12:04:44 +0000

    Rework tags

--- groups/storage/locals.tf
+++ groups/storage/locals.tf
@@ -1,7 +1,8 @@
 locals {
   common_tags = {
     Environment = var.environment
-    Service     = var.service
+    Provisioner = "Terraform"
     Repository  = var.repository
+    Service     = var.service
   }
 }
