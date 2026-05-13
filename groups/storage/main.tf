24e0083cd3c7768726fbdeb50ca52f382fba4a75
Marc Ransome <mransome@companieshouse.gov.uk>
2026-05-13 09:46:12 +0100

    Bump provider versions

--- groups/storage/main.tf
+++ groups/storage/main.tf
@@ -1,18 +1,16 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.3.0, < 2.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0.0, < 6.44.1"
+      version = ">= 6.0, < 7.0"
     }
   }
+
+  backend "s3" {}
 }
 
 provider "aws" {
   region = var.region
-}
-
-terraform {
-  backend "s3" {}
 }
