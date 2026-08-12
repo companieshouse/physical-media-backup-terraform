ab444ef0cbf96b0a5056bce63597953f5aa1f6d3
Marc Ransome <mransome@companieshouse.gov.uk>
2026-08-12 13:25:08 +0100

    Disable S3 bucket keys

--- groups/storage/s3.tf
+++ groups/storage/s3.tf
@@ -14,7 +14,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
   bucket = aws_s3_bucket.data.id
 
   rule {
-    bucket_key_enabled = true
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
